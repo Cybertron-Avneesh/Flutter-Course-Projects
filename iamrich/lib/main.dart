aa2cf042296c00b29473279621c902c3cea6dae7
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-05-02 13:56:55 +0530

    I am rich

---
+++ iamrich/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: Text("I am Rich"),
+      backgroundColor: Colors.blueGrey,
+    ),
+    body: Center(
+      child: Image(
+        image: AssetImage('images/diamond.png'),
+      ),
+    ),
+    backgroundColor: Colors.green,
+  )));
+}
