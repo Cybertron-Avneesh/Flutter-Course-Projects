f17cac8af81bcbddc7e6a56d88c2cd071e531d78
Cybertron-Avneesh <iib2019010@iiita.ac.in>
2020-05-02 14:19:10 +0530

    Clima

---
+++ Clima-Flutter-master/Clima-Flutter-master/lib/screens/loading_screen.dart
@@ -0,0 +1,45 @@
+import 'package:clima/screens/location_screen.dart';
+import 'package:clima/services/weather.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const apiKey = '144e560438c84f2238d441e8a8002567';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var weatherData = await WeatherModel().getLocationWeather();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(locationWeather: weatherData);
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitChasingDots(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+      extendBody: null,
+    );
+  }
+}
+
+/* 
+    var temp = decodedData['main']['temp'];
+      var condition = decodedData['weather'][0]['id'];
+      var cityName = decodedData['name'];*/
